b85774074400168b4f7edd0bf486ac17d5444e81
Sultan Bazher <107263975+SultanCYB@users.noreply.github.com>
2023-06-26 18:15:48 +0300

    Radical change
    Many edits and updates to improve the program

--- prayer_time.py
+++ prayer_time.py
@@ -4,174 +4,250 @@
 # For web
 import requests, re
 
-# For notifications
+# For files
+import sys
+import os
+
+# For notifications and other things
 from plyer import notification
-from time import sleep, strftime
+import time
 
-# these are the paths of each city in the website I get the prayer times from
 
-cities = {
-    "makkah": "/c16/Makkah",  #
-    "jeddah": "/c13/Jeddah",
-    "riyadh": "/c10/Riyadh",
-    "jaizan": "/c12/Jaizan",
-    "al kharj": "/c6/Al+kharj",
-    "buraidah": "/c11/Buraidah",
-    "hafr albatin": "/c1967/Hafr+albatin"
-}
+def Errors(err, error_time):
+    '''this function deal with errors and log the error if it was repeated 3 times
 
-city = 'makkah' # enter the city name
+    for example : 
 
-city = city.lower()
+        If connection error happend, it will retry 3 times every 20 minutes
+        then it will log the error and close the program.'''
 
-def Send(name, time):
+    global _attempts
+    _attempts += 1
+
+    if _attempts == 3:
+
+        try:
+            print("Couldn't fix the problem, check the error log..")
+
+            with open(error_log, 'a') as file:
+
+                file.write(f'[ERROR] [{error_time}] -->  {err}\n\n')
+
+                file.write(f'Error type : {type(err).__name__}\n')
+                file.write(f'Line : {(sys.exc_info()[2]).tb_lineno}\n')
+                file.write(f'For additional help, you can contact me at : sultanxx575@gmail.com\n')
+
+                file.write(f"{'-'*80}\n")
+
+            sys.exit()
+
+        except Exception as e:
+            error_time_2 = time.strftime("%Y-%m-%d %H:%M", time.localtime())  # time of the error
+
+            print(
+                f"""The program was trying to write an error to the error log but it seems that another
+error appeared in the error log function, so this process has failed. These are the errors :
+
+
+-    error information of the error that faced the error log function are:      
+
+    error name : {e}
+    error time : {error_time_2}
+    error line : {(sys.exc_info()[2]).tb_lineno}
+    error type : {type(e).__name__}
+
+-   error information for the second error that happend somewhere that isn't in the error log function are:
+
+    error name : {err}
+    error time : {error_time}
+    error type : {type(err).__name__}
+
+
+    if you keep getting the same problem contact me at
+
+    sultanxx575@gmail.com"""
+            )
+
+            sys.exit()
+
+    time.sleep(1200)
+    return main()
+
+
+def PrayerNotification(name):
     '''Notifications function'''
     global _attempts
 
-    if strftime("%H:%M %p") == time:
-        pass
+    try:
 
-    else:
-        print('Error something is wrong with the time entered')
-        return
+        #------------------------------------------------------------------------------
+        if name == "c":  # Error in connection
+            return notification.notify(
+                app_name="أوقات الصلاة",
+                title="أوقات الصلاة",
+                message='تأكد من إتصالك بالإنترنت',
+                app_icon='',  # Icon path should be determined by you
+            )
 
-    if name == "e":  # Error in connection
-        notification.notify(
-            app_name="أوقات الصلاة",
-            title="أوقات الصلاة",
-            message='تأكد من إتصالك بالإنترنت',
-            app_icon='',  # Icon path should be determined by you
-        )
-        return error()
+        elif name == 'n':  # None or something else wrong with finding prayer times
+            return notification.notify(
+                app_name="أوقات الصلاة",
+                title="أوقات الصلاة",
+                message='هنالك مشكلة في البحث عن أوقات الصلاة',
+                app_icon='',  # Icon path should be determined by you
+            )
 
-    elif name == 'n':  # None
-        notification.notify(
-            app_name="أوقات الصلاة",
-            title="أوقات الصلاة",
-            message='هنالك مشكلة في البحث عن أوقات الصلاة',
-            app_icon='',  # Icon path should be determined by you
-        )
-        return error()
+        elif name == 'w':  # wrong city name
+            return notification.notify(
+                app_name="أوقات الصلاة",
+                title="أوقات الصلاة",
+                message='هنالك خطأ, تأكد من اسم المدينة\nError, check the city name',
+                app_icon='',  # Icon path should be determined by you
+            )
+        #------------------------------------------------------------------------------
 
-    elif name == 'wrong name':
-        return notification.notify(
-            app_name="أوقات الصلاة",
-            title="أوقات الصلاة",
-            message='هنالك خطأ, تأكد من اسم المدينة\nError, check the city name',
-            app_icon='',  # Icon path should be determined by you
-        )
+        _attempts = 0
 
-    _attempts = 0
-    # if the name isn't error or none then the tool probably worked, so here we reset attempts
-
-    if name == "الضحى":  # Al duha (currently al duha notification is not available)
-
-        sleep(1200)  # 20 minutes for al duha
-        return notification.notify(
-            app_name="أوقات الصلاة",
-            title="أوقات الصلاة",
-            message='اقترب وقت صلاة الضحى',
-            app_icon='',  # Icon path should be determined by you
-        )
-
-    else:
-
-        if strftime("%A") == "Friday":  # Friday prayer
+        if time.strftime("%A") == "Friday" and name == "الظهر":
             name = 'الجمعة'
 
-        return notification.notify(
-            app_name="أوقات الصلاة",
-            title="أوقات الصلاة",
-            message='حان الآن وقت صلاة ' + name,
-            app_icon='',  # Icon path should be determined by you
-        )
+        if name == "الضحى":  # Al duha
+
+            time.sleep(1500)  # 20 minutes and extra 5 minutes to avoid any errors from the site
+            return notification.notify(
+                app_name="أوقات الصلاة",
+                title="أوقات الصلاة",
+                message='اقترب وقت صلاة الضحى',
+                app_icon='',  # Icon path should be determined by a function
+            )
+
+        else:
+            # For the 5 prayers
+            # 1- Fajir
+            # 2- Al duhr (Al jumaah if it was friday)
+            # 3- Al asr
+            # 4- Al maghrib
+            # 5- Al isha
+
+            return notification.notify(
+                app_name="أوقات الصلاة",
+                title="أوقات الصلاة",
+                message='حان الآن وقت صلاة ' + name,
+                app_icon='',  # Icon path should be determined by a user
+            )
+    except Exception as e:
+        error_time = time.strftime("%Y-%m-%d %H:%M", time.localtime())
+        Errors(e, error_time)
 
 
 def main():
-    '''getting information from website, the time to get a notification'''
-    global city
-
+    '''getting the time of the prayer and calling the notifications function'''
     prayers_times = {}
 
     try:
+
         response = requests.get(f'https://saudi-arabia.prayertiming.net/en/{city}')
 
-    except:
-        Send('e', strftime("%H:%M %p"))
+    except Exception as e:
 
-    else:
-        # parenthesis removed
-        regex = re.compile(r'<td>([0-9]{2}:[0-9]{2} (?:AM|PM))</td>')  # regular expression to get
-        # the prayer times from the page
+        error_time = time.strftime("%Y-%m-%d %H:%M", time.localtime())
+        PrayerNotification('c')
+        Errors(e, error_time)
+
+    try:
+        # regular expression to get the prayer times from the page
+        regex = re.compile(r'<td>([0-9]{2}:[0-9]{2} (?:AM|PM))</td>')
 
         if regex.findall(response.text):
 
             response = regex.findall(response.text)
 
+            # Assigning the prayers names as key and the times as variables
             prayers_times["الفجر"] = response[0]
-            # prayers_times["الضحى"] = response[1]  don't include in project
+            prayers_times["الضحى"] = response[1]
             prayers_times["الظهر"] = response[2]
-            prayers_times["العصر"] = response[3]  # putting prayer times into dictionary
+            prayers_times["العصر"] = response[3]
             prayers_times["المغرب"] = response[4]
             prayers_times["العشاء"] = response[5]
 
-            counter = 0
             while True:
-                if counter == 5:  # if Al duha notification is available make it (6)
-
-                    counter = 0  # turning it into 0 to repeat the same cycle
-                    # when it reach (5), because the prayers are (5) so there is nothing
-                    # to increase for or i will get an index error
 
                 for prayer in prayers_times:
 
-                    if strftime("%H:%M %p") == prayers_times[prayer]:
+                    if time.strftime("%H:%M %p") == prayers_times[prayer]:
                         # checking if the current time equals that prayer time
-                        # if it does then it will call the notification function (Send)
-                        Send(name=prayer, time=prayers_times[prayer].strip())
+                        # if it does then it will call the notification function PrayerNotification()
+                        PrayerNotification(name=prayer)
+                        time.sleep(1200)  # let the program have a break for 30 minutes
 
-                counter += 1
+                time.sleep(1)
 
-                sleep(1)
+    except Exception as e:
 
-        else:
-            Send('n', strftime("%H:%M %p"))
+        error_time = time.strftime("%Y-%m-%d %H:%M", time.localtime())
+        PrayerNotification('n')
+
+        Errors(e, error_time)
 
 
-def error():
-    '''function to deal with errors'''
-    global _attempts
+''' cities manual : 
 
-    _attempts += 1
+- makkah
+- jeddah
+- riyadh
+- jaizan
+- al kharj
+- buraidah
+- hafr albatin
 
-    if _attempts == 3:
-        print("Couldn't fix the problem, closing the program..")
-        exit()
-        # If connection error happend, it will retry 3 times every 20 minutes
-        # then it will close the program if the problem didn't get fixed.
-    sleep(1200)
-    return main()
+'''
 
+# Optional, default is makkah
+city = 'makkah'  # city name, check the city manual above for more info
+
+# Optional, default is the directory of this file
+
+current_folder: str = re.search(r'(.+\\).+$', __file__)[1]
+
+# Optional, default is the directory of this file
+
+error_log: str = current_folder + 'error.log'
+
+# these are the paths of each city in the website I get the prayer times from
+
+cities = {
+    "makkah": "c16/Makkah",  #
+    "jeddah": "c13/Jeddah",
+    "riyadh": "c10/Riyadh",
+    "jaizan": "c12/Jaizan",
+    "al kharj": "c6/Al+kharj",
+    "buraidah": "c11/Buraidah",
+    "hafr albatin": "c1967/Hafr+albatin"
+}
 
 try:
-    city = cities[city]
+    city = cities[city.lower()]
 
-except KeyError:
+except KeyError as e:
+
+    error_time = time.strftime("%Y-%m-%d %H:%M", time.localtime())
 
     print(
         '''Either you entered the city name wrong or the city is not supported in the program
 
 contact me for adding new cities or if you have any problems or questions :
 
-my Twitter account : @SultanCYB
+Github account : https://github.com/SultanCYB
 my Email account : sultanxx575@gmail.com'''
     )
-    Send('wrong name', strftime("%H:%M %p"))
-    exit()
+
+    PrayerNotification('w')
+    Errors(e)
+
+    sys.exit()
 
 # global variable
-
-_attempts = 0
+_attempts: int = 0
 
 if __name__ == '__main__':
     main()
